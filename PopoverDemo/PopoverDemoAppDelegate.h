1f8360fe2b272b23cf4e77097872ed24bbaf81de
Josh Smith <flapplejacks@gmail.com>
2011-05-30 16:35:19 -0700

    Initial Commit

---
+++ PopoverDemo/PopoverDemoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  PopoverDemoAppDelegate.h
+//  PopoverDemo
+//
+//  Created by Josh Smith on 5/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PopoverDemoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
